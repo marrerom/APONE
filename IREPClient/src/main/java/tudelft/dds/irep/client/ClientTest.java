7b2501e503c7f41b9bd0c5597bd15bf80297ba1f
mmarrero <mmarrero@mmarrero-Latitude-E7470>
2017-07-17 18:06:24 +0200

    jackson test

--- IREPClient/src/main/java/tudelft/dds/irep/client/ClientTest.java
+++ IREPClient/src/main/java/tudelft/dds/irep/client/ClientTest.java
@@ -1,6 +1,7 @@
 package tudelft.dds.irep.client;
 
 import java.io.IOException;
+
 import java.net.URLEncoder;
 import java.nio.file.Paths;
 
@@ -19,10 +20,11 @@ import javax.ws.rs.core.Response;
 public class ClientTest {
 	
 		final static String context = "http://localhost:8080/IREPServices/"; 
+		final static String jerseyServices = "service";
 		
 		
 		public static Response testYAMLForm(Client client) throws IOException{
-			WebTarget base = client.target(context);
+			WebTarget base = client.target(context+jerseyServices);
 			WebTarget target =base.path("/definition/upload");
 			Invocation.Builder builder = target.request(MediaType.TEXT_PLAIN);
 			String nsName = "demo_namespace";
@@ -37,7 +39,7 @@ public class ClientTest {
 		
 		
 		public static Response testDefinitionUpdateMultivariate(Client client) throws IOException{
-			WebTarget base = client.target(context);
+			WebTarget base = client.target(context+jerseyServices);
 			WebTarget target =base.path("/definition/upload");
 			Invocation.Builder builder = target.request(MediaType.TEXT_PLAIN);
 			String nsName = "demo_namespace";
@@ -54,9 +56,81 @@ public class ClientTest {
 			System.out.println(res);
 			return res;
 		}
+		
+		public static Response testDefinitionUpdateJson(Client client) throws IOException {
+			WebTarget base = client.target(context+jerseyServices);
+			WebTarget target =base.path("/definition/upload");
+			Invocation.Builder builder = target.request(MediaType.TEXT_PLAIN);
+			String nsName = "demo_namespace";
+			String experimenter = "mmarrero";
+			String description = "desc of demo_namespace";
+			String unit = "user_guid";
+			String control_treatment = "exp_default";
+
+			//String path = Paths.get("resources", nsName + ".yaml").toFile().toString();
+			//String yaml = Utils.readStrFile(path);
+
+//			- definition: exp_default
+//			    assign: !planout |
+//			        page_size = 50;
+//			        full_details = false;
+//
+//			  # study effects of different page sizes only
+//			  - definition: exp_page_size
+//			    # PLanOut DSL, see http://facebook.github.io/planout/docs/planout-language-reference.html
+//			    assign: !planout |
+//			      page_size = uniformChoice(choices = [15, 20, 30], unit = user_guid);
+//
+//			  # study effects of both detail level and page sizes applied together
+//			  - definition: exp_everything
+//			    assign: !planout |
+//			      full_details = bernoulliTrial(p=0.3, unit=user_guid);
+//			      if (full_details) {
+//			        page_size = weightedChoice(choices = [10, 15, 20], weights = [0.25, 0.5, 0.25], unit = user_guid);
+//			      } else {
+//			        page_size = weightedChoice(choices = [20, 25, 30], weights = [0.25, 0.5, 0.25], unit = user_guid);
+//			      }
+
+			String t_name1 = "exp_default";
+			String t_definition1 = "page_size = 50;full_details = false;";
+			String t_description1 = "treatment desc 1";
+			
+			String treatment1 =	"{\"name\":\""+t_name1+"\",\"definition\":\""+t_definition1+"\","
+					+ "\"description\":\""+t_description1+"\"}";
+			
+			String t_name2 = "exp_page_size";
+			String t_definition2 = "page_size = uniformChoice(choices = [15, 20, 30], unit = user_guid);";
+			String t_description2 = "treatment desc 2";
+			
+			String treatment2 =	"{\"name\":\""+t_name2+"\",\"definition\":\""+t_definition2+"\","
+					+ "\"description\":\""+t_description2+"\"}";		
+
+			String t_name3 = "exp_everything";
+			String t_definition3 = " full_details = bernoulliTrial(p=0.3, unit=user_guid);" + 
+					"      if (full_details) {" + 
+					"        page_size = weightedChoice(choices = [10, 15, 20], weights = [0.25, 0.5, 0.25], unit = user_guid);" + 
+					"      } else {" + 
+					"        page_size = weightedChoice(choices = [20, 25, 30], weights = [0.25, 0.5, 0.25], unit = user_guid);" + 
+					"      }";
+			String t_description3 = "treatment desc 3";
+			
+			String treatment3 =	"{\"name\":\""+t_name3+"\",\"definition\":\""+t_definition3+"\","
+					+ "\"description\":\""+t_description3+"\"}";		
+
+			
+			
+			String experiment = "{\"name\":\""+nsName+"\",\"experimenter\":\""+experimenter+"\","
+					+ "\"description\":\""+description+"\",\"unit\":\""+unit+"\","
+							+ "\"control_treatment\":\""+control_treatment+"\",\"treatment\":["+treatment1+","+treatment2+","+treatment3+"]}";
+			
+			Response res = builder.post(Entity.entity(experiment, MediaType.APPLICATION_JSON));
+			System.out.println(res);
+			return res;
+
+		}
 
 		public static Response testGet(Client client) {
-		   WebTarget base = client.target(context);
+		   WebTarget base = client.target(context+jerseyServices);
 		   WebTarget target =base.path("/get");
 		   Invocation.Builder builder = target.request(MediaType.TEXT_PLAIN);
 		   Response res = builder.get(); //also builder.get(String.class) to return only the string
@@ -65,7 +139,7 @@ public class ClientTest {
 	   }
 		
 		public static Response testConfigurationStart(Client client, String idexp){
-			WebTarget base = client.target(context);
+			WebTarget base = client.target(context+jerseyServices);
 			WebTarget target =base.path("/configuration/start");
 			Invocation.Builder builder = target.request(MediaType.TEXT_PLAIN);
 			MultivaluedMap<String, String> mmap = new MultivaluedHashMap<String, String>();
@@ -76,7 +150,7 @@ public class ClientTest {
 		}
 		
 		public static Response testRunGetParams(Client client, String idrun, String idunit){
-			WebTarget base = client.target(context);
+			WebTarget base = client.target(context+jerseyServices);
 			WebTarget target =base.path("/run/getParams");
 			Invocation.Builder builder = target.request(MediaType.TEXT_PLAIN);
 			MultivaluedMap<String, String> mmap = new MultivaluedHashMap<String, String>();
@@ -90,19 +164,18 @@ public class ClientTest {
 	   public static void main(String[] args) throws Exception{
 		   Client client = ClientBuilder.newClient();
 		   try{
-		   //testGet(client);
-		   Response res1 =  testDefinitionUpdateMultivariate(client);
+		   Response res1 =  testDefinitionUpdateJson(client);
 		   String idexp = res1.readEntity(String.class);
 		   
-		   Response res2 = testConfigurationStart(client, idexp);
-		   String idrun = res2.readEntity(String.class);
-		   
-		   for (int i=0;i<5;i++){
-			   String unitid = String.valueOf(i);
-			   Response res3 = testRunGetParams(client, idrun, unitid);
-			   String params = res3.readEntity(String.class);
-			   System.out.println("Exp "+ idexp + " Run " +idrun +" Unit "+unitid +" Params "+params);
-		   }
+//		   Response res2 = testConfigurationStart(client, idexp);
+//		   String idrun = res2.readEntity(String.class);
+//		   
+//		   for (int i=0;i<5;i++){
+//			   String unitid = String.valueOf(i);
+//			   Response res3 = testRunGetParams(client, idrun, unitid);
+//			   String params = res3.readEntity(String.class);
+//			   System.out.println("Exp "+ idexp + " Run " +idrun +" Unit "+unitid +" Params "+params);
+//		   }
 		   
 		   client.close();
 		   } catch (Exception e){
