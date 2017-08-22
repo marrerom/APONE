37fbe96752d56abb4e594106c5ddc47405ac4d50
marrerom <mmarrero@mmarrero-Latitude-E7470>
2017-08-22 19:19:11 +0200

    get experiment service

--- IREPServices/src/tudelft/dds/irep/services/Experiment.java
+++ IREPServices/src/tudelft/dds/irep/services/Experiment.java
@@ -11,6 +11,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
@@ -143,6 +144,22 @@ public class Experiment {
 		}
 	}
 	
+	@Path("/get/{idrun}")
+	@GET
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String register(@PathParam("idrun") String idrun) {
+		try {
+			ExperimentManager em = (ExperimentManager)context.getAttribute("ExperimentManager");
+			JExperiment jexp = em.getExperimentFromConf(idrun);
+			ObjectMapper mapper = new ObjectMapper();
+			String expstr = mapper.writeValueAsString(jexp); 
+			return expstr;
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+			throw new javax.ws.rs.BadRequestException(e.getCause().getMessage());
+		}
+	}
 	
 	@Path("/getParams")
 	@POST
